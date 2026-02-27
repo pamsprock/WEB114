9d31c95779fc3095b653610d4fed12de6d8b3d7c
pamsprock <pamsprock@hotmail.com>
2026-02-27 14:06:36 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,16 @@
+//Pamela Sprock 02-24-26
+"use-strict";
+let myName = 'Pamela Sprock';
+let myFavoriteColor = 'purple';
+let myFavoriteHobby = 'reading';
+let myFavoriteFood = 'chocolate';
+let myFavoriteAnimal = 'dog';
+let myFavoriteSeason = 'summer';
+let myFavoriteRestaurant = 'Garozzos'
+
+const output = "Hello. My name is " + myName + ", and my favorite color is " +
+myFavoriteColor + ".  I enjoy " + myFavoriteHobby + ", " + myFavoriteFood + ", and " +
+myFavoriteSeason + ". My favorite animial is a " + myFavoriteAnimal + ", and I " +
+"enjoy eating at " + myFavoriteRestaurant +".";
+
+console.log(output);
