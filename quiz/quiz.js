bf80859e922638860fac9a59f4ff1ebdf6c476a8
pamsprock <pamsprock@hotmail.com>
2026-03-12 16:39:27 -0500

    Add files via upload

---
+++ quiz/quiz.js
@@ -0,0 +1,42 @@
+//Pamela Sprock 03-12-2026
+"use strict";
+
+//Ask and evaluate question 1
+let answer1 = prompt("What is the capital of France?");
+
+if (answer1.toLowerCase() === "paris") {
+    console.log("You are correct!");
+}
+else {
+    console.log("Sorry. The answer is Paris.");
+}
+
+//Ask and evaluate question 2
+let answer2 = parseInt(prompt("What is 6+7?"));
+
+if (answer2 === 13) {
+    console.log("You are correct!");
+}
+else {
+    console.log("Sorry. The answer is 13.");
+}
+
+//Ask and evaluate question 3
+let answer3 = prompt("Where does SpongeBob live?");
+
+if (answer3.toLowerCase() === "in a pineapple under the sea") {
+    console.log("You are correct!");
+}
+else {
+    console.log("Sorry. The answer is in a pineapple under the sea.");
+}
+
+//Ask and evaluate question 4
+let answer4 = parseInt(prompt("What is the square root of 64?"));
+
+if (answer4 === 8) {
+    console.log("You are correct!");
+}
+else {
+    console.log("Sorry. The answer is 8.")
+}
